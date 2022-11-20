12082823a7b073c2540d0e53317209520c364cca
sravanigandikota <106865387+sravanigandikota@users.noreply.github.com>
2022-11-20 15:04:46 +0530

    Add files via upload

--- JavaScriptAssignment 1/Digital Clock/script1.js
+++ JavaScriptAssignment 1/Digital Clock/script1.js
@@ -1,29 +1,35 @@
 function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
+    let time = new Date();
+    let hour = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    am_pm = "AM";
+
+    if(hour>12){
+        hour = hour-12;
+        am_pm = "PM";
+
     }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
+    if(hour == 0){
+        hour = 12;
+        am_pm = "AM";
+
     }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+   
+    let currentTime = hour + ":" + minutes + ":" + seconds +" "+ am_pm;
+      
+    document.getElementById("clock").innerHTML = currentTime;
 }
 
-showTime();
+var newInterval;
+function startClock(){
+     newInterval = setInterval(showTime, 1000); 
+}
+
+function stopClock(){
+   clearInterval(newInterval);
+}
